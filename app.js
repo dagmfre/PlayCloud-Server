779bd0f1d6a844ecc348c21b8449af524485b62a
dagmfre <dagmfre@gmail.com>
2024-03-22 00:37:22 +0300

    Automate refresh token accessing & Store newly generated random state to db

--- app.js
+++ app.js
@@ -28,15 +28,37 @@ app.use(
 app.use(express.json());
 app.use(cors());
 
+// Defining the schema
+const authorizationSchema = new mongoose.Schema({
+  access_token: String,
+  refresh_token: String,
+  expires_in: Number,
+  scope: String,
+  state: String,
+});
+
+//  Defining model
+const authorizationModel = mongoose.model(
+  "authorizationData",
+  authorizationSchema
+);
+
+// Routes
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
 app.get("/login", function (req, res) {
   var state = generateRandomString(30);
+  
+  // Storing newly generated random state to db
+  const newState = new authorizationModel({
+    state: state,
+  });
+  newState.save();
+
   const scope =
     "user-read-private user-read-email user-library-read user-library-modify streaming user-modify-playback-state user-read-playback-state user-read-currently-playing user-read-recently-played user-top-read";
-  const redirect_uri = "http://localhost:3001/callback";
 
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
@@ -44,7 +66,7 @@ app.get("/login", function (req, res) {
         response_type: "code",
         client_id: process.env.CLIENT_ID,
         scope: scope,
-        redirect_uri: redirect_uri,
+        redirect_uri: process.env.REDIRECT_URI,
         state: state,
       })
   );
@@ -66,7 +88,7 @@ app.get("/callback", function (req, res) {
       url: "https://accounts.spotify.com/api/token",
       form: {
         code: code,
-        redirect_uri: "http://localhost:3001/callback",
+        redirect_uri: process.env.REDIRECT_URI,
         grant_type: "authorization_code",
       },
       headers: {
@@ -80,13 +102,14 @@ app.get("/callback", function (req, res) {
       json: true,
     };
     axios
-      .post(authOptions.url, querystring.stringify(), {
+      .post(authOptions.url, querystring.stringify(authOptions.form), {
         headers: authOptions.headers,
       })
       .then((response) => {
         if (response.status === 200) {
-          console.log(response.data.access_token);
-          console.log(response.data.refresh_token);
+          //   console.log(response.data.access_token);
+          //   console.log(response.data.refresh_token);
+          console.log(response.data);
           res.send(response.data.access_token);
         } else {
           console.error("Failed to exchange code for access token.");
@@ -96,6 +119,37 @@ app.get("/callback", function (req, res) {
         console.error("Error exchanging code for access token:", error);
       });
   }
+});
+
+app.get("/refresh_token", function (req, res) {
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        new Buffer.from(
+          process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
+        ).toString("base64"),
+    },
+    form: {
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
+    },
+    json: true,
+  };
+
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token,
+        refresh_token = body.refresh_token;
+      res.send({
+        access_token: access_token,
+        refresh_token: refresh_token,
+      });
+    }
+  });
 });
 
 app.listen(3001, function () {
